956c16f11bd3434683c6fef361a528120ae6c84e
flix <1590184@qq.com>
2023-08-22 11:18:14 +0800

    update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,17 +2,19 @@ import { fileURLToPath, URL } from "node:url";
 
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import vueJsx from "@vitejs/plugin-vue-jsx";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   define: {
-    global: {},
-  },
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    global: {
       process: "process/browser",
     },
   },
+  plugins: [vue(), vueJsx()],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+  base: "./",
 });
